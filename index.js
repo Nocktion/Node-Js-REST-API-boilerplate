4278d7f212ad86500cf2305966359c8d20455734
Nocktion <dominik.hagyba@nockdev.net>
2022-03-14 16:58:21 +0100

    Added MySQL example

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const mysql = require("../db/mysql.js");
+const mssql = require("../db/mssql.js");
 
 const app = express();
 
@@ -28,19 +30,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/example/:id', (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
+    
+    mysql.query(`select name, age from exampleTable where id=${id}`, (err, result) => {
+        if (err) console.log(err);
+        
+        res.json({ "name": result[0].name, "age": result[0].age });
+    };
 });
 
 app.post('/example', (req, res) => {
-  
+    var name = req.body.name;
+    var age = req.body.age;
 });
 
 app.delete('/example/:id', (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 });
 
 app.patch('/example/:id', (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 });
 
 const PORT = process.env.PORT || 8080;
